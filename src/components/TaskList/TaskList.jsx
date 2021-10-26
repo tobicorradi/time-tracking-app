5154ba6097b223e4e815db00cb78b6afdfb13a35
Tobias <corraditobias@gmail.com>
2021-10-25 22:49:40 -0300

    Total time added

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { formatTime } from '../../Utils'
 const TaskList = ({list}) => {
-
+    const getAllTime = () => {
+      return list.reduce((acc, el) => { return acc + el.time},0)
+    }
     return (
         <section className="task-list mt-5">
+            <span className="d-block mb-3">Tiempo total <strong>{formatTime(getAllTime(list))}</strong></span>
             {list.map(el => (
                 <div className="task d-flex justify-content-between p-4 shadow-sm mb-3">
                     <span>{el.text}</span>
